10062a93f9a2231663f052ffbb7e2f186c0bd8e8
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-11-28 23:35:53 -0500

    commit tree 372cc9aa96cf874f5759657e6deadb9780c0600d

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,11 @@
 export function manageFriends(state, action){
+	switch (action.type) {
+		case 'ADD_FRIEND':
+			// return Object.assign(state.friends, action.friend)
+			return {friends: [...state.friends, action.friend]}
+		case 'REMOVE_FRIEND':
+			return {friends: state.friends.filter(friend => friend.id !== action.id)}
+		default:
+			return state;
+	}
 }
